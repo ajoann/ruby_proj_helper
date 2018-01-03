4c81d5b9257389971735056356f248c0cc2cdfb9
Amanda <ajhwhiz@gmail.com>
2018-01-03 10:30:35 -0800

    editing server

--- routes.js
+++ routes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const Particle = require('particle-api-js');
 const particle = new Particle();
 
-router.get('/',(req,res) => {
-	console.log("thanks");
-	res.send(200);
-})
+// router.get('/',(req,res) => {
+// 	console.log("thanks");
+// 	res.send(200);
+// })
 
 let USERNAME = process.env.PARTICLE_USERNAME;
 let PASSWORD = process.env.PARTICLE_PASSWORD;
